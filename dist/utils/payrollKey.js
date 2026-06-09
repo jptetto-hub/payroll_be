271b3f83bc75ccb16921e89d6ca9a7b6f5f45cdb
Jayaprakash Srinivasan <jp@Jayaprakashs-MacBook-Air.local>
2026-06-09 17:22:11 +0530

    remove pre-built dist folder

--- dist/utils/payrollKey.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.buildActivePayrollKey = buildActivePayrollKey;
-const formatDate = (date) => date.toISOString().slice(0, 10);
-function buildActivePayrollKey(params) {
-    return `${params.employeeId}_${formatDate(params.periodStart)}_${formatDate(params.periodEnd)}`;
-}
-//# sourceMappingURL=payrollKey.js.map
--- dist/utils/serializeBigInt.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.serializeBigInt = serializeBigInt;
-function serializeBigInt(data) {
-    return JSON.parse(JSON.stringify(data, (_key, value) => typeof value === "bigint" ? Number(value) : value));
-}
-//# sourceMappingURL=serializeBigInt.js.map
